1e0441334b729e03e7ec3cbab1c530fea7624212
nodgear <wbteuz@gmail.com>
2017-11-14 04:47:37 -0200

    Login index
    Mudança de estilo, optmização e redução de classes

--- login.php
+++ login.php
@@ -1,48 +1,60 @@
 <?php include('kernel/head.php') ?>
 <style type="text/css">
-    body {
-      background-color: #DADADA;
-    }
     body > .grid {
-      height: 100%;
-    }
-    .image {
-      margin-top: -100px;
+      height: 60%;
     }
     .column {
-      max-width: 450px;
+      max-width: 500px;
     }
   </style>
 <body>
+  <div class="ui secondary inverted menu" id="nav">
+  <a class="item" href="index.php">
+    <img src="css/images/logo.png"
+  </a>
+  <a class="item">
+    Personagens
+</a>
+  <a class="item">
+    Criadores
+  </a>
+
+<a class="active item" href="index.php">
+Início
+</a>
+
+
+</div>
 
 <div class="ui middle aligned center aligned grid">
   <div class="column">
-    <h2 class="ui teal image header">
-      <img src="css/images/logo.png" class="image">
-    </h2>
+    <h1 class="ui image header" id="login">
+      <img src="css/images/logo.png" class="big image">
+      <br>BATTLE<font color="#f38426">GUIDE</font>
+    </h1>
     <form class="ui large form" method="post" action="index.php" name="loginform">
 
-        <div class="field">
+        <div class="field" id="userform">
           <div class="ui left icon input">
             <i class="user icon"></i>
             <input placeholder="Usuário" id="login_input_username" class="login_input" type="text" name="user_name" required />
           </div>
         </div>
-        <div class="field">
+        <div class="field" id="passform">
           <div class="ui left icon input">
             <i class="lock icon"></i>
             <input placeholder="Senha" id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
           </div>
         </div>
-        <input type="submit"  name="login" value="Log in" class="ui fluid button" />
+        <input type="submit"  name="login" value="Log in" class="ui fluid button" / id="logform">
 
 
       <div class="ui error message"></div>
 
     </form>
 
-    <div class="ui inverted message">
-  
+    <div class="ui inverted message" id="regform">
+
       Novo por aqui? <a href="register.php">Registre-se</a>
     </div>
   </div>
