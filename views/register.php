5295679a1b2d0adfbd9234d8f4fcea34433a46d6
nodgear <wbteuz@gmail.com>
2017-11-14 04:50:46 -0200

    POST registro

--- views/register.php
+++ views/register.php
@@ -1,4 +1,73 @@
-<?php
+<?php include('kernel/head.php') ?>
+<!-- register form -->
+<style type="text/css">
+    body > .grid {
+      height: 80%;
+    }
+    .column {
+      max-width: 500px;
+    }
+  </style>
+<body>
+  <div class="ui secondary inverted menu" id="nav">
+  <a class="item" href="index.php">
+    <img src="css/images/logo.png"
+  </a>
+  <a class="item">
+    Personagens
+</a>
+  <a class="item">
+    Criadores
+  </a>
+
+<a class="active item" href="index.php">
+Início
+</a>
+
+
+</div>
+
+<div class="ui middle aligned center aligned grid">
+  <div class="column">
+    <h1 class="ui image header" id="login">
+      <img src="css/images/logo.png" class="image">
+      <br>BATTLE<font color="#f38426">GUIDE</font>
+    </h1>
+    <form class="ui large form" method="post" action="register.php" name="registerform">
+
+        <div class="field" id="userform">
+          <label>Somente letras e números, no mínimo 4 caracteres.</label>
+          <div class="ui left icon input">
+            <i class="user icon"></i>
+            <input placeholder="Usuário" id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
+          </div>
+        </div>
+        <div class="field" id="userform">
+          <div class="ui left icon input">
+            <i class="mail icon"></i>
+            <input placeholder="E-mail" id="login_input_email" class="login_input" type="email" name="user_email" required />
+          </div>
+        </div>
+        <div class="field" id="passform">
+          <label>Mínimo 6 caracteres.</label>
+          <div class="ui left icon input">
+            <i class="lock icon"></i>
+            <input placeholder="Senha" id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
+          </div>
+        </div>
+        <div class="field" id="passform">
+          <div class="ui left icon input">
+            <i class="lock icon"></i>
+            <input placeholder="Repetir senha" id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
+          </div>
+        </div>
+        <input type="submit"  name="register" value="Register" class="ui fluid button" id="logform"/>
+
+
+      <div class="ui error message"></div>
+
+    </form>
+<div class="ui inverted message" id="regform"><?php
 // show potential errors / feedback (from registration object)
 if (isset($registration)) {
     if ($registration->errors) {
@@ -6,33 +75,17 @@ if (isset($registration)) {
             echo $error;
         }
     }
+  }
+  if (isset($registration)) {
     if ($registration->messages) {
         foreach ($registration->messages as $message) {
             echo $message;
         }
     }
 }
-?>
-
-<!-- register form -->
-<form method="post" action="register.php" name="registerform">
-
-    <!-- the user name input field uses a HTML5 pattern check -->
-    <label for="login_input_username">Username (only letters and numbers, 2 to 64 characters)</label>
-    <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
-
-    <!-- the email input field uses a HTML5 email type check -->
-    <label for="login_input_email">User's email</label>
-    <input id="login_input_email" class="login_input" type="email" name="user_email" required />
-
-    <label for="login_input_password_new">Password (min. 6 characters)</label>
-    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
-
-    <label for="login_input_password_repeat">Repeat password</label>
-    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
-    <input type="submit"  name="register" value="Register" />
-
-</form>
-
-<!-- backlink -->
-<a href="index.php">Back to Login Page</a>
+?></div>
+    <div class="ui inverted message" id="regform">
+Já possui uma conta? <a href="login.php">Conecte-se</a>
+    </div>
+  </div>
+</div>
